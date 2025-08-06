af83fd239dafadb014b2ce4fe34e121157058346
GitHub Action <action@github.com>
2025-08-06 08:33:27 +0000

    Initialize template repository proper
    Set package name to com.github.johnzudeml213.uncclientb2
    Set project name to uncclientb2
    Set modid to uncclientb

--- src/main/java/com/github/johnzudeml213/uncclientb2/init/AutoDiscoveryMixinPlugin.java
+++ src/main/java/com/github/johnzudeml213/uncclientb2/init/AutoDiscoveryMixinPlugin.java
@@ -1,4 +1,4 @@
-package com.example.init;
+package com.github.johnzudeml213.uncclientb2.init;
 
 import org.spongepowered.asm.lib.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
--- src/main/java/com/github/johnzudeml213/uncclientb2/mixin/MixinGuiMainMenu.java
+++ src/main/java/com/github/johnzudeml213/uncclientb2/mixin/MixinGuiMainMenu.java
@@ -1,4 +1,4 @@
-package com.example.mixin;
+package com.github.johnzudeml213.uncclientb2.mixin;
 
 import net.minecraft.client.gui.GuiMainMenu;
 import org.spongepowered.asm.mixin.Mixin;
